83f916851540ae1c186604a7d8678ede72f38a61
maying <maying@luojilab.com>
2020-03-31 18:59:08 +0800

    订单模块重构-xx

--- src/search.js
+++ src/search.js
@@ -1,1 +1,17 @@
-document.write('search page!!') 
+// document.write('search page!!') 
+
+import React from 'react';
+import ReactDom from 'react-dom'
+import './search.css'
+
+class Search extends React.Component{
+    render(){
+        return <div className="search-text">react search</div>
+    }
+}
+
+
+ReactDom.render(
+    <Search/>,
+    document.getElementById('root')
+)
